b0629576dcb50813a75cedbf44ce6b4d1ba398f5
Rahul Rokade <your_email@example.com>
2026-07-14 14:35:16 +0530

    Fast commit of updated agent, config, project metadata, and lockfile

---
+++ agent/agentic_work_flow.py
@@ -0,0 +1,23 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class GraphBuider():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def beil_graph(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    
